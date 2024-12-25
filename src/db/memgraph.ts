63fb3fbd753127cf1a8c60b9f6dcca9e5e7a5629
YirenNing24 <paulanthonyarriola@gmail.com>
2024-12-25 22:58:26 +0800

    updates

--- src/db/memgraph.ts
+++ src/db/memgraph.ts
@@ -1,34 +1,59 @@
 import neo4j, { Driver } from 'neo4j-driver';
+import { NEO4J_PASSWORD, NEO4J_URI, NEO4J_USERNAME }  from '../config/constants';
 
-let driver: Driver | undefined;
+let driver: Driver;
 
-export async function initDriver(uri: string, username: string, password: string): Promise<Driver> {
-  driver = neo4j.driver(
-    uri,
-    neo4j.auth.basic(
-      username,
-      password
-    )
-  );
+async function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-  await driver.getServerInfo()
-  return driver;
+export async function initDriver(uri: string, username: string, password: string, retries: number = 5, delay: number = 2000): Promise<Driver> {
+  let attempt: number = 0;
+
+  while (attempt < retries) {
+    try {
+      driver = neo4j.driver(
+        uri,
+        neo4j.auth.basic(username, password),
+        // {
+        //   encrypted: 'ENCRYPTION_ON',  // Enable SSL
+        //   trust: 'TRUST_ALL_CERTIFICATES',
+        // }
+      );
+
+      // Try to get server info to verify the connection
+      await driver.getServerInfo();
+
+      // If connection is successful, break out of the loop
+      console.log('Successfully connected to Memgraph');
+      return driver;
+    } catch (error: any) {
+      attempt++;
+      console.error(`Attempt ${attempt} failed: ${error.message}`);
+
+      if (attempt < retries) {
+        console.log(`Retrying in ${delay / 1000} seconds...`);
+        await sleep(delay);
+      } else {
+        throw new Error(`Failed to connect to Neo4j after ${retries} attempts.`);
+      }
+    }
+  }
+
+  // This will never be reached if the retry limit is reached and an error is thrown
+  throw new Error('Unable to initialize Neo4j driver.');
 }
 
 /**
- * Get the instance of the Neo4j Driver created in the
- * `initDriver` function
- *
- * @returns {neo4j.Driver | undefined}
+ * Get the instance of the Neo4j Driver created in the `initDriver` function
+ * @returns {neo4j.Driver}
  */
-export function getDriver(): Driver | undefined {
+export function getDriver(): Driver {
   return driver;
 }
 
 /**
- * If the driver has been instantiated, close it and all
- * remaining open sessions
- *
+ * If the driver has been instantiated, close it and all remaining open sessions
  * @returns {Promise<void>}
  */
 export async function closeDriver(): Promise<void> {
@@ -36,14 +61,4 @@ export async function closeDriver(): Promise<void> {
     await driver.close();
   }
 }
-
-
-
-//** INIT */
-
-//** CREATE CARD STORE NODE  */
-//** CONSTRAINT ONLY ONE CARD STORE NODE */
-
-//** CARD ID CONSTRAINT */
-//** CARD ID CONSTRAINT */
 
